7dfd39445e015cfdaba48f6ce4f830a15df6b3f5
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-01-16 20:57:40 +0700

    refactor

---
+++ command_help.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, v := range getCommands() {
+		fmt.Printf("%s: %s\n", v.name, v.description)
+	}
+	return nil
+}
--- repl.go
+++ repl.go
@@ -15,15 +15,15 @@ type cliCommand struct {
 
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
 		"help": {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
 		},
 	}
 }
@@ -46,23 +46,6 @@ func startRepl() {
 			fmt.Println("Unknown command")
 		}
 	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println()
-	for _, v := range getCommands() {
-		fmt.Printf("%s: %s\n", v.name, v.description)
-	}
-	return nil
 }
 
 func cleanInput(text string) []string {
